405ea19d277ccf15b3933b8043298d9b18c3756c
Juliorex23 <per221023@uvg.edu.gt>
2024-08-25 20:41:36 -0400

    Update main.cpp
    Configuración de pines en el set up

--- main.cpp
+++ main.cpp
@@ -9,11 +9,10 @@
 #include "config.h"
 #include "display7seg.h"
 
-// Definición de pines para los dígitos
+// Definiciones de pines y configuración
 #define DIS1 0
 #define DIS2 2
 #define DIS3 15
-// Definición de segmentos 
 #define sA 32
 #define sB 33
 #define sC 26
@@ -22,82 +21,59 @@
 #define sF 14
 #define sG 27
 #define sP 13
-
-// Definición de canales PWM para los LEDs
 #define canalPWM_R 10
 #define canalPWM_V 11
 #define canalPWM_A 6
-
-// Definición de canal PWM para el servo
 #define canalPWM_Servo 9
-
-// Frecuencia en Hz
 #define freqPWM 50
 #define freqPWMLED 50
-
-// Resolución del duty cycle en bits
 #define resPWM 10
 #define resPWMLED 10
-
-// Definición de los pines para los LEDs
 #define LEDR 23
 #define LEDA 22
 #define LEDV 1
-
-// Definiciones para el control del sensor de temperatura 
 #define ADC_VREF_mV 3300.0
 #define ADC_RESOLUCION 4096.0
-
-// Definición de pines y variables
 #define lm35Pin 36
 #define BTN1 18
-
-// Definición del pin del servo motor
 #define pinServo 21
-
-// Definición de valores PWM para el Servo
 #define PWM_min 26
 #define PWM_max 128
-
-// Definiciones de PWM para los LEDs
 #define PWM_minLed 0
 #define PWM_maxLed 250
 
-// Variable de control de LEDs
 int currentfunction = 0;
-
-// Variable de estado pasado para controlar el servo
 int estadopas = 0;
 int reloj;
 float temperature; 
 
-volatile bool enviar = false; // Bandera que indica si se debe leer el sensor
+volatile bool enviar = false; 
 
-// Funciones ISR para los botones
 void IRAM_ATTR onButton1Press() {
     enviar = true;
 }
 
-// Función de inicialización de PWMs
-void initPWMLEDs(void);
-void initPWMServo(void);
+void initPWMLEDs(void) {
+    ledcSetup(canalPWM_R, freqPWMLED, resPWMLED);
+    ledcAttachPin(LEDR, canalPWM_R);
+    ledcWrite(canalPWM_R, 0);
+    ledcSetup(canalPWM_V, freqPWMLED, resPWMLED);
+    ledcAttachPin(LEDV, canalPWM_V);
+    ledcWrite(canalPWM_V, 0);
+    ledcSetup(canalPWM_A, freqPWMLED, resPWMLED);
+    ledcAttachPin(LEDA, canalPWM_A);
+    ledcWrite(canalPWM_A, 0);
+}
 
-// Función para controlar el servo motor
-void setServoPosition(void);
+void initPWMServo(void) {
+    ledcSetup(canalPWM_Servo, freqPWM, resPWM);
+    ledcAttachPin(pinServo, canalPWM_Servo);
+    ledcWrite(canalPWM_Servo, PWM_min);
+}
 
-// Función para controlar el botón
-void button (void);
-
-// Funciones para controlar el display 
-void mostrador();
-void temporizacion();
-void controlLEDs (void);
-
-// Configuración inicial
 void setup() {
     Serial.begin(115200);
     while (!Serial);
-
     Serial.print("Connecting to Adafruit IO");
     io.connect();
     while (io.status() < AIO_CONNECTED) {
@@ -106,6 +82,16 @@ void setup() {
     }
     Serial.println();
     Serial.println(io.statusText());
+    pinMode (DIS1, OUTPUT);
+    pinMode (DIS2, OUTPUT);
+    pinMode (DIS3, OUTPUT);
+    pinMode (lm35Pin, INPUT);
+    pinMode (LEDR, OUTPUT);
+    pinMode (LEDA, OUTPUT);
+    pinMode (LEDV, OUTPUT);
+    initPWMLEDs();
+    button();
+    initPWMServo();
 }
 void loop() {
 }
